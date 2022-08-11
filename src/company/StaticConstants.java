afff0b39ce2456b5cf77f0444bddae227022359f
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-08-10 21:41:17 -0400

    Stored customer object

---
+++ src/company/StaticConstants.java
@@ -0,0 +1,11 @@
+package company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StaticConstants {
+
+    public static final List<Customer> CUSTOMER_LIST = new ArrayList<>();
+
+
+}
