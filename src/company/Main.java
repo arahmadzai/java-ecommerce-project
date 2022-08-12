999a764055c4b380c17c6281a9f66251862aaa7f
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-08-11 22:15:44 -0400

    imported the scanner so we can pick our customer

--- src/company/Main.java
+++ src/company/Main.java
@@ -1,5 +1,7 @@
 package company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,8 +10,16 @@ public class Main {
         DataGenerator.createCategory();
         DataGenerator.createProduct();
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Select Customer:");
+        for(int i = 0; i< StaticConstants.CUSTOMER_LIST.size(); i++){
+            System.out.println("Type " + i + " for customer:" + StaticConstants.CUSTOMER_LIST.get(i).getUserName());
+        }
 
+        Customer customer = StaticConstants.CUSTOMER_LIST.get(scanner.nextInt());
+
+        System.out.println(customer.getUserName());
 
     }
 
