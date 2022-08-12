f90ad9ff0c23717d21b6f0ee9e2e8e31bcf09ba4
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-08-11 22:30:56 -0400

    Case 0, Categories list created

--- src/company/Main.java
+++ src/company/Main.java
@@ -1,5 +1,7 @@
 package company;
 
+import company.category.Category;
+
 import java.util.Scanner;
 
 public class Main {
@@ -13,7 +15,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Select Customer:");
-        for(int i = 0; i< StaticConstants.CUSTOMER_LIST.size(); i++){
+        for (int i = 0; i < StaticConstants.CUSTOMER_LIST.size(); i++) {
             System.out.println("Type " + i + " for customer:" + StaticConstants.CUSTOMER_LIST.get(i).getUserName());
         }
 
@@ -21,8 +23,56 @@ public class Main {
 
         System.out.println(customer.getUserName());
 
+        while (true) {
+
+            System.out.println("What would you like to do? Just type id for selection");
+
+            for (int i = 0; i < prepareMenuOptions().length; i++) {
+                System.out.println(i + "-" + prepareMenuOptions()[i]);
+            }
+
+            int menuSelection = scanner.nextInt();
+
+            switch (menuSelection){
+
+                case 0: //list categories
+                    for(Category category : StaticConstants.CATEGORY_LIST){
+                        System.out.println("Category Code:" + category.generateCategoryCode() + " category name:" + category.getName());
+                    }
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+
+
+
+
+
+            }
+
+        }
+
+
     }
 
-
+    private static String[] prepareMenuOptions() {
+        return new String[]{"List Categories", "List Products", "List Discount", "See Balance", "Add Balance",
+                "Place an order", "See Cart", "See order details", "See your address", "Close App"};
+    }
 
 }
